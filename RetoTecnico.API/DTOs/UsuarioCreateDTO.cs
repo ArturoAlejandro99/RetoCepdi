f20f539a90a47165022ac01bfc02e365aabdc777
ArturoAlejandro27 <69064240+ArturoAlejandro27@users.noreply.github.com>
2025-07-22 00:02:08 -0600

    Manejo de caracteres especiales

--- RetoTecnico.API/DTOs/UsuarioCreateDTO.cs
+++ RetoTecnico.API/DTOs/UsuarioCreateDTO.cs
@@ -10,7 +10,7 @@ public class UsuarioCreateDto
 
     [Required(ErrorMessage = "La contraseña es obligatoria.")]
     [StringLength(100, MinimumLength = 8, ErrorMessage = "La contraseña debe tener entre 8 y 100 caracteres.")]
-    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$",
                        ErrorMessage = "La contraseña debe contener al menos una mayúscula, una minúscula, un número y un carácter especial.")]
     public required string Password { get; set; }
 
