1ea3d6f9c57f88bc4d5d1d5049d214f0c2ba6310
ArturoAlejandro27 <69064240+ArturoAlejandro27@users.noreply.github.com>
2025-07-21 22:28:16 -0600

    Clases databae

---
+++ RetoTecnico.API/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RetoTecnico.API.Models;
+
+public partial class Usuario
+{
+    public int Idusuario { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public DateTime? Fechacreacion { get; set; }
+
+    public string? NombreUsuario { get; set; }
+
+    public string? Password { get; set; }
+
+    public int? Idperfil { get; set; }
+
+    public int? Estatus { get; set; }
+}
