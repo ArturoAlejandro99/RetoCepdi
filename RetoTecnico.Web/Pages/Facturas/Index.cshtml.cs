3887f2aa2bd39c4e4a3b67a0c01772bbeb4d6f5b
ArturoAlejandro27 <69064240+ArturoAlejandro27@users.noreply.github.com>
2025-07-24 00:41:47 -0600

    Facturación

--- RetoTecnico.Web/Pages/Facturas/Index.cshtml.cs
+++ RetoTecnico.Web/Pages/Facturas/Index.cshtml.cs
@@ -1,8 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 
-namespace RetoTecnico.Web.Pages.Usuarios
+namespace RetoTecnico.Web.Pages.Facturas
 {
     public class IndexModel : PageModel
     {
